5784e021f4b1c27821cbabc93dfe8cbda4a9c57c
mriduava <mriduava@gmail.com>
2020-09-04 19:51:38 +0200

    add initial search function for mathem in backend

--- server/server.js
+++ server/server.js
@@ -62,6 +62,58 @@ const { app } = require('mongoosy')({
    return res.send(products);
  });
 
+ //Get by product name
+ app.get('/api/harvestMathem/:name', async(req, res)=>{
+   let productName = req.params.name
+   let products = [];
+   let dataHarvest = await fetch(
+     `https://api.mathem.io/product-search/noauth/search/query?size=25&searchType=searchResult&type=p&sortTerm=popular&sortOrder=desc&storeId=10&q=` + productName
+   ).then((data) => data.json());
+   dataHarvest = dataHarvest.products
+   dataHarvest.map(product => {
+               let dataProduct = new Product({
+                 productName: product.name,
+                 productFullName: product.fullName,
+                 volume: `${product.quantity} ${product.unit}`,
+                 url: product.url,
+                 retail: "mathem",
+                 label:
+                   product.badges.length > 1
+                     ? `${product.badges.forEach((badge) => {
+                         return badge.name;
+                       })}`
+                     : null,
+                 origin: product.origin ? product.origin.name : "Not specified",
+                 ecologic:
+                   product.badges.length > 1
+                     ? product.badges.forEach((badge) => {
+                         //badge.name == "Ekologisk" ? true : false;
+                       })
+                     : false,
+                 priceUnit: product.unit,
+                 price: product.price,
+                 comparePrice: product.comparisonPrice,
+                 compareUnit: product.comparisonUnit,
+                 discount: product.discount
+                   ? {
+                       memberDiscount: product.discount ? true : false,
+                       prePrice: product.discount ? product.price : null,
+                       discountPrice: product.discount
+                         ? product.discount.price
+                         : null,
+                       maxQuantity: product.discount
+                         ? product.discount.quantityToBeBought
+                         : null,
+                     }
+                   : null,
+               });
+               //console.log(dataProduct);
+               products.push(dataProduct)
+               dataProduct.save()
+              })
+   return res.json(products);
+ })
+
 //Example of product to save in MongoDB
 
 app.get("/api/mathem", (req, res) => {
--- src/Pages/homePage.js
+++ src/Pages/homePage.js
@@ -4,10 +4,33 @@ import { Button, Form, FormGroup, Label, Input } from 'reactstrap'
 
 const HomePage = () => {
 
+    const [search, setSearch] = useState('')
+
+    const searchProduct = async (input) => {
+      let res;
+      if(!input.trim()) {
+        res = await fetch('api/harvestMathem')
+      } else {
+        res = await fetch('/rest/recipes/search/' + input)
+      }
+
+      res = await res.json()
+    }
+
     return (
       <div className="container-fluid">
         Home page
         {/* <SearchField /> */}
+
+        <FormGroup className="col-10">
+        <Input 
+          onChange={e => searchProduct(e.target.value)}
+          type="text" 
+          id="searching" 
+          autoComplete="off"
+          placeholder="search recipe ..." />
+      </FormGroup>
+      <Button onClick={searchProduct}>Search</Button>
       </div>
     );
 }
