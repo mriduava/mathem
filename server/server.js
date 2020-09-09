bf7c236a42074a3b8062484e2bfd964a88680631
Martin Hellström <martver12@gmail.com>
2020-09-09 09:13:56 +0200

    reworked mathem harvester to a method

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const { app } = require('mongoosy')({
   }
 });
 
- app.get("/api/harvestMathem", async (req, res) => {
+ const mathemHarvester = () => {
    let products = [];
    let categories = [
      "frukt-o-gront",
@@ -31,72 +31,74 @@ const { app } = require('mongoosy')({
      "smaksattare",
      "djurmat-o-tillbehor",
      "kiosk",
-    ]
-    categories.forEach(async (category) => {
-     let dataCategory = new Category({name: category})
-               Category.find(
-                 { name: category},
-                 (err, result) => {
-                   if (!result.length) {
-                     dataCategory.save();
-                   } else {
-                     dataCategory.update();
-                   }
-                 }
-               );
-      let dataHarvest = await fetch(
-     `https://api.mathem.io/product-search/noauth/search/products/10/categorytag/${category}?size=1000&storeId=10&searchType=category&sortTerm=popular&sortOrder=desc`
-   ).then((data) => data.json());
-   dataHarvest = dataHarvest.products
-   dataHarvest.map(product => {
-               let dataProduct = new Product({
-                 productName: product.name,
-                 productFullName: product.fullName,
-                 volume: `${product.quantity} ${product.unit}`,
-                 url: product.url,
-                 retail: "mathem",
-                 label:
-                   product.badges.length > 1
-                     ? `${product.badges.forEach((badge) => {
-                         return badge.name;
-                       })}`
-                     : null,
-                 origin: product.origin ? product.origin.name : "Not specified",
-                 ecologic:
-                   product.badges.length > 1
-                     ? product.badges.forEach((badge) => {
-                         badge.name == "Ekologisk" ? true : false;
-                       })
-                     : false,
-                 priceUnit: product.unit,
-                 price: product.price,
-                 comparePrice: product.comparisonPrice,
-                 compareUnit: product.comparisonUnit,
-                 discount: product.discount
-                   ? {
-                       memberDiscount: product.discount ? true : false,
-                       prePrice: product.discount ? product.price : null,
-                       discountPrice: product.discount
-                         ? product.discount.price
-                         : null,
-                       maxQuantity: product.discount
-                         ? product.discount.quantityToBeBought
-                         : null,
-                     }
-                   : null,
-               });
-               products.push(dataProduct)
-               Product.find({productFullName : dataProduct.productFullName}, (err, result) => {
-                 if(!result.length){
-                  dataProduct.save()
-                 }
-                 else{
-                  dataProduct.update()
-                 }
+   ];
+   categories.forEach(async (category) => {
+     let dataCategory = new Category({ name: category });
+     Category.find({ name: category }, (err, result) => {
+       if (!result.length) {
+         dataCategory.save();
+       } else {
+         dataCategory.update();
+       }
+     });
+     let dataHarvest = await fetch(
+       `https://api.mathem.io/product-search/noauth/search/products/10/categorytag/${category}?size=1000&storeId=10&searchType=category&sortTerm=popular&sortOrder=desc`
+     ).then((data) => data.json());
+     dataHarvest = dataHarvest.products;
+     dataHarvest.map((product) => {
+       let dataProduct = new Product({
+         productName: product.name,
+         productFullName: product.fullName,
+         volume: `${product.quantity} ${product.unit}`,
+         url: product.url,
+         retail: "mathem",
+         label:
+           product.badges.length > 1
+             ? `${product.badges.forEach((badge) => {
+                 return badge.name;
+               })}`
+             : null,
+         origin: product.origin ? product.origin.name : "Not specified",
+         ecologic:
+           product.badges.length > 1
+             ? product.badges.forEach((badge) => {
+                 badge.name == "Ekologisk" ? true : false;
                })
-              })
-    })
- });
+             : false,
+         priceUnit: product.unit,
+         price: product.price,
+         comparePrice: product.comparisonPrice,
+         compareUnit: product.comparisonUnit,
+         discount: product.discount
+           ? {
+               memberDiscount: product.discount ? true : false,
+               prePrice: product.discount ? product.price : null,
+               discountPrice: product.discount ? product.discount.price : null,
+               maxQuantity: product.discount
+                 ? product.discount.quantityToBeBought
+                 : null,
+             }
+           : null,
+       });
+       products.push(dataProduct);
+       Product.find(
+         { productFullName: dataProduct.productFullName },
+         (err, result) => {
+           if (!result.length) {
+             dataProduct.save();
+           } else {
+             dataProduct.update();
+           }
+         }
+       );
+     });
+   });
+ }
+
+ const dailyDataHarvest = () => {
+   
+ }
+
 
 app.get("/api/mathem/:name",async (req, res) => {
     await Product.find(
