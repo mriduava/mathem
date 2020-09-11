664292c7ae0c54cbf047299632d8dc06a222f59f
Martin Hellström <martver12@gmail.com>
2020-09-11 11:51:58 +0200

    Completed transition to class for mathem

--- server/MathemHarvester.js
+++ server/MathemHarvester.js
@@ -1,109 +1,108 @@
 const mathemProduct = require("./models/mathemProduct");
 const Category = require("./models/Category");
+const fetch = require("node-fetch");
 
-const Mathem = () => {
-    harvester = () => {
-        databaseCategories().forEach(async category => {
-             let dataHarvest = await fetch(
-      `https://api.mathem.io/product-search/noauth/search/products/10/categorytag/${category}?size=1000&storeId=10&searchType=category&sortTerm=popular&sortOrder=desc`
-    ).then((data) => data.json());
-    console.log(category);
-    dataHarvest = dataHarvest.products;
-    scrubber(dataHarvest)
-    console.log("Harvest complete");
-        })
-};
-}
- const mathem = () => {
-    return new Mathem()
-};
-scrubber = (dataharvest) => {
-        dataHarvest.map((product) => {
-      let getLabels = () => {
-        let labels = "";
-        product.badges.forEach((badge) => {
-          if (badge.name !== "Ekologisk") {
-            labels += badge.name + ", ";
-          }
-        });
-        if (labels === "") {
-          labels = "No label";
-        }
-        return labels;
-      };
-      let dataProduct = new mathemProduct({
-        productName: product.name,
-        productFullName: product.fullName.toLowerCase(),
-        volume: `${product.quantity} ${product.unit}`,
-        url: product.url,
-        image: product.images.MEDIUM,
-        retail: "mathem",
-        label: getLabels(),
-        origin: product.origin ? product.origin.name : "Not specified",
-        ecologic:
-          product.badges.length >= 1
-            ? product.badges.forEach((badge) => {
-                return badge.name === "Ekologisk" ? true : false;
-              })
-            : false,
-        priceUnit: product.unit,
-        price: product.price,
-        comparePrice: product.comparisonPrice,
-        compareUnit: product.comparisonUnit,
-        discount: product.discount
-          ? {
-              memberDiscount: product.discount ? true : false,
-              prePrice: product.discount ? product.price : null,
-              discountPrice: product.discount ? product.discount.price : null,
-              maxQuantity: product.discount
-                ? product.discount.quantityToBeBought
-                : null,
-            }
-          : null,
-      });
-      mathemProduct.find(
-        { productFullName: dataProduct.productFullName.toLowerCase() },
-        (err, result) => {
-          if (!result.length) {
-            dataProduct.save();
-          } else {
-            dataProduct.update();
-          }
-        }
-      );
-    });
-}
+ class Mathem {
+   harvester = () => {
+     let categories = this.databaseCategories();
+     categories.forEach(async (category) => {
+       let dataHarvest = await fetch(
+         `https://api.mathem.io/product-search/noauth/search/products/10/categorytag/${category}?size=1000&storeId=10&searchType=category&sortTerm=popular&sortOrder=desc`
+       ).then((data) => data.json());
+       dataHarvest = dataHarvest.products;
+       this.scrubber(dataHarvest);
+     });
+   };
+   databaseCategories = () => {
+     let categories = [
+       "frukt-o-gront",
+       "mejeri-o-ost",
+       "brod-o-bageri",
+       "kott-o-chark",
+       "dryck",
+       "skafferi",
+       "fisk-o-skaldjur",
+       "hem-o-hygien",
+       "fardigmat-o-halvfabrikat",
+       "glass-godis-o-snacks",
+       "barnmat-o-tillbehor",
+       "apotek-o-halsa",
+       "smaksattare",
+       "djurmat-o-tillbehor",
+       "kiosk",
+     ];
+     categories.forEach(async (category) => {
+       let dataCategory = new Category({
+         name: category,
+         categoryId: null,
+         retailName: "Mathem",
+       });
+       Category.find({ name: category }, (err, result) => {
+         if (!result.length) {
+           dataCategory.save();
+         } else {
+           dataCategory.update();
+         }
+       });
+     });
+     return categories;
+   };
 
- databaseCategories = () => {
-          let categories = [
-    "frukt-o-gront",
-    "mejeri-o-ost",
-    "brod-o-bageri",
-    "kott-o-chark",
-    "dryck",
-    "skafferi",
-    "fisk-o-skaldjur",
-    "hem-o-hygien",
-    "fardigmat-o-halvfabrikat",
-    "glass-godis-o-snacks",
-    "barnmat-o-tillbehor",
-    "apotek-o-halsa",
-    "smaksattare",
-    "djurmat-o-tillbehor",
-    "kiosk",
-  ];
-  categories.forEach(async (category) => {
-    let dataCategory = new Category({
-      name: category,
-      categoryId: null,
-      retailName: "Mathem",
-    });
-    Category.find({ name: category }, (err, result) => {
-      if (!result.length) {
-        dataCategory.save();
-      } else {
-        dataCategory.update();
-      }
-    });
-    return categories
-    })}
+   scrubber = (dataHarvest) => {
+     dataHarvest.map((product) => {
+       let getLabels = () => {
+         let labels = "";
+         product.badges.forEach((badge) => {
+           if (badge.name !== "Ekologisk") {
+             labels += badge.name + ", ";
+           }
+         });
+         if (labels === "") {
+           labels = "No label";
+         }
+         return labels;
+       };
+       let dataProduct = new mathemProduct({
+         productName: product.name,
+         productFullName: product.fullName.toLowerCase(),
+         volume: `${product.quantity} ${product.unit}`,
+         url: product.url,
+         image: product.images.MEDIUM,
+         retail: "mathem",
+         label: getLabels(),
+         origin: product.origin ? product.origin.name : "Not specified",
+         ecologic:
+           product.badges.length >= 1
+             ? product.badges.forEach((badge) => {
+                 return badge.name === "Ekologisk" ? true : false;
+               })
+             : false,
+         priceUnit: product.unit,
+         price: product.price,
+         comparePrice: product.comparisonPrice,
+         compareUnit: product.comparisonUnit,
+         discount: product.discount
+           ? {
+               memberDiscount: product.discount ? true : false,
+               prePrice: product.discount ? product.price : null,
+               discountPrice: product.discount ? product.discount.price : null,
+               maxQuantity: product.discount
+                 ? product.discount.quantityToBeBought
+                 : null,
+             }
+           : null,
+       });
+       mathemProduct.find(
+         { productFullName: dataProduct.productFullName.toLowerCase() },
+         (err, result) => {
+           if (!result.length) {
+             dataProduct.save();
+           } else {
+             dataProduct.update();
+           }
+         }
+       );
+     });
+   };
+ }
+module.exports = Mathem
