00515c97cd8c5848c76dc02d237d4d419a690c9a
Martin Hellström <martver12@gmail.com>
2020-09-14 12:16:52 +0200

    Added comments for guidelines

--- server/Shopping.js
+++ server/Shopping.js
@@ -2,11 +2,12 @@ const fetch = require("node-fetch");
 const Cart = require('./models/Cart')
 
 class Shopping{
+    //Implement cart registration to db to simulate cart checkout
     createCart(){
-
+        
     }
     
+    //Calculate price and compare with other stores of similar product
     calculatePrice(){
-
     }
 }
