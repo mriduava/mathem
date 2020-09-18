681e8ed912bc2bc0bc112f001a3b6b9e22ffb196
AAntonSB <anton.s.bergqvist@gmail.com>
2020-09-18 12:43:18 +0200

    Filter for price range

--- server/PrioritySystem.js
+++ server/PrioritySystem.js
@@ -1,4 +1,30 @@
-module.exports = class FilterManager {
+let numArr = [
+  3467,
+  1501,
+  11178,
+  1502,
+  1504,
+  1510,
+  1505,
+  1448,
+  1511,
+  1508,
+  1515,
+  1512,
+  1513,
+  1506,
+  1493,
+  11197,
+  1503,
+  1507,
+  1514,
+  1509,
+  3970,
+  2680,
+  3473,
+];
+
+class FilterManager {
   countingSort = (arr, min, max) => {
     let i = min,
       j = 0,
@@ -28,6 +54,13 @@ module.exports = class FilterManager {
   // Priorities products with discounts
 
   // Return products in price range
+  filterPriceRange = (arr, min, max) => {
+    return arr.filter((element) => min < element && element < max);
+  };
 
   // Sort by price
-};
+}
+
+const filter = new FilterManager();
+
+console.log("test");
