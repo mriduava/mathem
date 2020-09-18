3bfc5bc7dd9f26cd41c3420f8d99d183d9726a02
AAntonSB <anton.s.bergqvist@gmail.com>
2020-09-18 12:32:29 +0200

    ecological filter

--- server/PrioritySystem.js
+++ server/PrioritySystem.js
@@ -1,20 +1,33 @@
-let countingSort = (arr, min, max) => {
-  let i = min,
-    j = 0,
-    len = arr.length,
-    count = [];
-  for (i; i <= max; i++) {
-    count[i] = 0;
-  }
-  for (i = 0; i < len; i++) {
-    count[arr[i]] += 1;
-  }
-  for (i = min; i <= max; i++) {
-    while (count[i] > 0) {
-      arr[j] = i;
-      j++;
-      count[i]--;
+module.exports = class FilterManager {
+  countingSort = (arr, min, max) => {
+    let i = min,
+      j = 0,
+      len = arr.length,
+      count = [];
+    for (i; i <= max; i++) {
+      count[i] = 0;
     }
-  }
-  return arr;
+    for (i = 0; i < len; i++) {
+      count[arr[i]] += 1;
+    }
+    for (i = min; i <= max; i++) {
+      while (count[i] > 0) {
+        arr[j] = i;
+        j++;
+        count[i]--;
+      }
+    }
+    return arr;
+  };
+
+  // Return Ecological values
+  filterEcological = (arr) => {
+    return arr.filter((element) => element.ecological === true);
+  };
+
+  // Priorities products with discounts
+
+  // Return products in price range
+
+  // Sort by price
 };
