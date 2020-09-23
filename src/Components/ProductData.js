ff234b9e8966a3f361551298e3ef8a9632cf055d
Martin Hellström <martver12@gmail.com>
2020-09-23 15:16:06 +0200

    Remade productlist products into component

--- src/Components/ProductData.js
+++ src/Components/ProductData.js
@@ -1,78 +1,22 @@
 import React, { useState, useContext } from "react";
-import {  Row, Col, CardImg, Modal, ModalBody, Input } from "reactstrap";
+import {  Row, Col, CardImg, Modal, ModalBody, Input, Button } from "reactstrap";
 import { ProductContext } from "../contexts/ProductContextProvider";
+import Product from './Product'
 
 const ProductMap = ({products}, props) => {
-  const { productList, updateProductList, productInfo, getProductInfo } = useContext(ProductContext);
+  const { productInfo} = useContext(ProductContext);
   const [modal, setModal] = useState(false);
   const toggle = () => setModal(!modal);
-
-  const [count, setCount] = useState(0);
-
-  const operators = {
-    "+": function (a) {
-      return a + 1;
-    },
-    "-": function (a) {
-        setCount(count--)
-      return a - 1;
-    },
-  };
-
-  const addProduct = (product, operator) => {
-    const matchingProduct = productList.find(
-      (x) => x.productFullName === product.productFullName
-    );
-    if (matchingProduct !== undefined) {
-      matchingProduct.quantity = operators[operator](
-        matchingProduct.quantity
-      );
-    } else {
-      product.quantity = 1;
-      updateProductList(product);
-    }
-  };
 
   return(
     <div>
       {products ? 
       products.map((product, i) => {
       return (
-        <Row key={product._id + i}
-          style={{margin: '5px 0', padding: '1px 0', border: '1px solid #ddd'}}>
-          <Col xs="10" sm="10" onClick={()=>{toggle(); getProductInfo(product._id);}}>
-            <Row style={{cursor: 'pointer', backgroundColor: 'rgb(247 247 255)', padding: '1.2% 0'}}>
-              <Col xs="1" sm="1">
-                <div style={{width: '60px', height: '60px', backgroundSize: 'cover', overflow: 'hidden'}}>
-                  <CardImg top width="100%" src={product.image} alt="Card image cap"/>
-                </div>
-              </Col>
-              <Col xs="6" sm="6">
-                <h4 style={{color:'#424242'}} className="ml-3">{product.productName}</h4>
-                <p style={{textTransform: 'uppercase'}} className="ml-3">{product.retail}</p>
-              </Col>
-              <Col xs="2" sm="2">
-                <h5 style={{color:'#FA5858'}}>{product.price} :-</h5>
-              </Col>
-            </Row>
-          </Col>
-          <Col xs="2" sm="2" style={{textAlign: 'right', backgroundColor: 'rgb(234 234 234)'}} 
-            onClick={() => addProduct(product,'+')}>
-              <div className="d-flex justify-content-end" style={{margin: '15px 0 0 0'}}>
-                <div style={{color: 'red'}}>
-                  <h2><i className="fas fa-minus-circle"></i></h2>
-                </div>
-                <div style={{margin: '2px 5px 0 5px', width:"65px"}}>
-                  <Input type="number" min="0" max="100" placeholder="0" value={count}/>
-                </div>
-                <div style={{color: 'green'}}>
-                  <h2><i className="fas fa-plus-circle"></i></h2>
-                </div>
-              </div>
-          </Col>
-          <hr/> 
-        </Row>
-      )
+        <div key={product._id + i}>
+          <Product product={product} i={i} toggle={toggle}/>
+        </div>
+      );
     }) : null}
       <Modal isOpen={modal} toggle={toggle} size="lg">
         <ModalBody>
@@ -92,17 +36,17 @@ const ProductMap = ({products}, props) => {
               <p style={{color: '#294360', fontSize: '16px', marginLeft: '20px'}}>Rabatt: {productInfo.discount ? productInfo.discount.memberDiscount : null}</p>
               </div>
               <p style={{color: '#294360', fontSize: '24px', textTransform: 'uppercase', margin: '5px 0 0 0'}}>{productInfo.retail}</p>        
-              <div className="d-flex" style={{margin: '18px 0 0 0'}}>
+              {/* <div className="d-flex" style={{margin: '18px 0 0 0'}}>
                 <div style={{color: 'red'}}>
                   <h1 onClick={() => addProduct(productInfo,'+')}><i className="fas fa-minus-circle"></i></h1>
                 </div>
                 <div style={{margin: '6px 5px 0 5px', width:"65px"}}>
-                  <Input type="number" min="0" max="100" placeholder="0" />
+                  <Input type="number" min="0" max="100" placeholder="0" value={productInfo.quantity} />
                 </div>
                 <div style={{color: 'green'}}>
                   <h1 onClick={() => addProduct(productInfo,'-')}><i className="fas fa-plus-circle"></i></h1>
                 </div>
-              </div>
+              </div> */}
             </div>
           </Col>
         </Row>
