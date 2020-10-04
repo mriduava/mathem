f080c5d4720c04bdda33dd5f5bfa4172ec0aec26
AAntonSB <anton.s.bergqvist@gmail.com>
2020-10-04 13:02:01 +0200

    Fixed spaces for null objects

--- src/Components/ComparedResults.js
+++ src/Components/ComparedResults.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import { Row, Col, Card, CardImg, Container, Button } from "reactstrap";
+import { Row, Col, Card, CardImg, Container } from "reactstrap";
 import { ProductContext } from "../contexts/ProductContextProvider";
 import "../CSS/comparedList.css";
 
@@ -26,16 +26,16 @@ const ComparedResults = () => {
     if (length <= 0) return;
     for (let i = 0; i < length; ++i) {
       const bestPrices = findAllValues(keys, i, objList, "price");
-      const bestBulkPrices = findAllValues(keys, i, objList, "kgPrice");
       updateLocalCompareList((objList[bestPrices][i].bestValue = true));
-      updateLocalCompareList((objList[bestBulkPrices][i].bestBulkValue = true));
+      // const bestBulkPrices = findAllValues(keys, i, objList, "kgPrice");
+      // updateLocalCompareList((objList[bestBulkPrices][i].bestBulkValue = true));
     }
   };
 
   const findAllValues = (retailors, index, objOfArrsOfObj, value) => {
     const values = retailors.map((retailor) => {
       const product = objOfArrsOfObj[retailor][index];
-
+      if (product === null) return Infinity;
       return typeof product === "object" && value in product
         ? product[value]
         : Infinity;
@@ -44,6 +44,7 @@ const ComparedResults = () => {
 
     return retailors.filter((retailor) => {
       const product = objOfArrsOfObj[retailor][index];
+      if (product === null) return false;
       return value in product ? product[value] === minPrice : false;
     });
   };
@@ -60,59 +61,72 @@ const ComparedResults = () => {
               <h1>{prettifyRetailor[retail]}</h1>
               <div>
                 {products.map((product, j) => {
-                  //TODO reuse code above to check max length of the longest array and create dummy ones for correct
-                  return (
-                    <Container
-                      key={j}
-                      style={{
-                        maxHeight: "300px",
-                        marginBottom: "20px",
-                        paddingBottom: "20px",
-                      }}
-                    >
-                      <Col>
-                        <div className="center">
-                          <Card
-                            style={{ height: "300px", paddingBottom: "30px" }}
-                            className="link"
-                            onClick={() => window.open(product.url)}
-                          >
-                            <div>
+                  console.log(product);
+                  if (product !== null) {
+                    return (
+                      <Container
+                        key={j}
+                        style={{
+                          Height: "300px",
+                          marginBottom: "20px",
+                          paddingBottom: "20px",
+                        }}
+                      >
+                        <Col>
+                          <div className="center">
+                            <Card
+                              style={{ height: "300px", paddingBottom: "30px" }}
+                              className="link"
+                              onClick={() => window.open(product.url)}
+                            >
                               <div>
                                 <div>
-                                  <CardImg
-                                    src={product.image}
-                                    top
-                                    style={{ maxHeight: "200px" }}
-                                  />
-                                  <div className="centerText">
-                                    <h5>{product.productName}</h5>
+                                  <div>
+                                    <CardImg
+                                      src={product.image}
+                                      top
+                                      style={{ maxHeight: "200px" }}
+                                    />
+                                    <div className="centerText">
+                                      <h5>{product.productName}</h5>
+                                    </div>
                                   </div>
                                 </div>
-                              </div>
 
-                              <b>{product.price} kr</b>
-                              <div className="centerText">
-                                {product.bestValue ? (
-                                  <h6>
-                                    Denna produkten är billigast!{" "}
-                                    {checkMarkEmoji}
-                                  </h6>
-                                ) : (
-                                  ""
-                                )}
-                                {product.bestBulkValue ? (
-                                  <h6>Bästa kilo priset! {checkMarkEmoji}</h6>
-                                ) : (
-                                  ""
-                                )}
+                                <b>{product.price} kr</b>
+                                <div className="centerText">
+                                  {product.bestValue ? (
+                                    <h6>
+                                      Denna produkten är billigast!{" "}
+                                      {checkMarkEmoji}
+                                    </h6>
+                                  ) : (
+                                    ""
+                                  )}
+                                  {product.bestBulkValue ? (
+                                    <h6>Bästa kilo priset! {checkMarkEmoji}</h6>
+                                  ) : (
+                                    ""
+                                  )}
+                                </div>
                               </div>
-                            </div>
-                          </Card>
-                        </div>
-                      </Col>
-                    </Container>
-                  );
+                            </Card>
+                          </div>
+                        </Col>
+                      </Container>
+                    );
+                  } else {
+                    return (
+                      <Container
+                        key={j}
+                        style={{
+                          Height: "300px",
+                          marginBottom: "20px",
+                          paddingBottom: "20px",
+                        }}
+                      ></Container>
+                    );
+                  }
                 })}
               </div>
             </Col>
